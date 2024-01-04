2aab4adeb0af019bc8178843f3dbaa1143f2f4a4
JUIC3zxy <juleszxy@gmail.com>
2024-01-04 21:18:13 +1100

    signup form set up

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,21 +1,25 @@
-import {Outlet,Navigate } from 'react-router-dom'
+import { Outlet, Navigate } from "react-router-dom";
 
-const AuthLayout = () => {
-  const isAutherized =false
+
+export default function AuthLayout() {
+const isAuthenticated=false;
   return (
     <>
-      {isAutherized ? (
+      {isAuthenticated ? (
         <Navigate to="/" />
-      ) : (<><section className='flex flex-1 justify-center flex-col py-10'>
-          <Outlet />
-        </section>
-          <img src='../public\assets\images\side-img.svg'
-            alt='logo'
-            className='hidden xl:block h-screen w-1/2 object-cover bg-no-repeat'/>
+      ) : (
+        <>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet />
+          </section>
+
+          <img
+            src="/assets/images/side-img.svg"
+            alt="logo"
+            className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+          />
         </>
       )}
     </>
-  )
+  );
 }
-
-export default AuthLayout
